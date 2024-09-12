c6abf203231431d970f11b71802585b83521b52f
Flavio Álvarez Dorantes <154248755+flavioAlvarezD@users.noreply.github.com>
2024-09-12 13:08:46 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 #webScraperEnv\Scripts\activate OR source webScraperEnv/bin/activate
 #pip install selenium, pandas, ipython, plotly, flask
 #pip install -U kaleido
-
+#pip install openpyxl
 
 #For MacOs:
 #sudo spctl --master-disable
