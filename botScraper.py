06710c4cba0eb85424f8e256f3e3d586948a8e79
Flavio Álvarez Dorantes <154248755+flavioAlvarezD@users.noreply.github.com>
2024-09-18 16:42:51 -0600

    Update botScraper.py

--- botScraper.py
+++ botScraper.py
@@ -18,6 +18,12 @@ options.add_argument("--disable-popup-blocking") # Evades popups
 options.add_argument("--start-maximized") # Specify launch options and resolution to assure consistency on results
 options.add_argument("--window-size=1920x1080")
 
+# Reads the selected excel file and takes the info on the columns
+#uploadFolder = 'uploads/'
+#files = os.listdir(uploadFolder)
+#filePath = os.path.join(uploadFolder, files[0])
+#df = pd.read_excel(filePath, engine='openpyxl')
+
 df = pd.read_excel('uploads/uploaded.xlsx')
 
 searchQueries = df.iloc[:, 0].tolist()
